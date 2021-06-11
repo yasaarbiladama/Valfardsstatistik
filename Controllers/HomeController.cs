73cac1703c5dd5e7a836fff8460c0839b0eef470
YB <yasaar.biladama@devlight.se>
2021-06-11 13:15:29 +0200

    new version

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using ChartJSCore.Helpers;
+using ChartJSCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Valfardsstatistik.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace Valfardsstatistik.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+
+            Chart pieChart = GeneratePieChart();
+            ViewData["PieChart"] = pieChart;
+
+            return View();
+        }
+
+        private Chart GeneratePieChart()
+        {
+            Chart chart = new Chart();
+            chart.Type = Enums.ChartType.Pie;
+
+            Data data = new Data();
+            data.Labels = new List<string>() { "Red", "Blue", "Yellow" };
+
+            PieDataset dataset = new PieDataset()
+            {
+                Label = "My dataset",
+                BackgroundColor = new List<ChartColor>() {
+                    ChartColor.FromHexString("#FF6384"),
+                    ChartColor.FromHexString("#36A2EB"),
+                    ChartColor.FromHexString("#FFCE56")
+                },
+                HoverBackgroundColor = new List<ChartColor>() {
+                    ChartColor.FromHexString("#FF6384"),
+                    ChartColor.FromHexString("#36A2EB"),
+                    ChartColor.FromHexString("#FFCE56")
+                },
+                 Data = new List<double?>() { 300, 50, 100 }
+                
+            };
+
+            
+
+            data.Datasets = new List<Dataset>();
+            data.Datasets.Add(dataset);
+
+            chart.Data = data;
+
+            return chart;
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Valfardsstatistik.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
